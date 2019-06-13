f024ccc4dadf084aae1f3c6ff58cff8fa48ebfd9
cjsong <157151004@qq.com>
2019-06-13 17:05:59 +0800

    删除多余的依赖模块

--- createProject.js
+++ createProject.js
@@ -2,23 +2,11 @@
 
 const chalk = require('chalk');
 const commander = require('commander');
-const dns = require('dns');
-const envinfo = require('envinfo');
-const execSync = require('child_process').execSync;
 const fs = require('fs-extra');
-const hyperquest = require('hyperquest');
-const inquirer = require('inquirer');
 const os = require('os');
 const path = require('path');
-const semver = require('semver');
-const spawn = require('cross-spawn');
-const tmp = require('tmp');
-const unpack = require('tar-pack').unpack;
-const url = require('url');
 const validateProjectName = require('validate-npm-package-name');
-
 const packageJson = require('./package.json');
-
 const errorLogFilePatterns = [
     'npm-debug.log',
 ];
